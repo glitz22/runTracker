85bf66e9eff79f3ea9d48d018037ac4c624ac389
glitz22 <marcgarlitz@gmail.com>
2016-02-17 23:41:54 -0500

    expanded functionality of authentication service

--- client/common/services/authentication.service.js
+++ client/common/services/authentication.service.js
@@ -3,8 +3,8 @@
 		.module("runTrackerApp")
 		.service("authentication", authentication);
 
-	authentication.$inject = ["$window"];
-	function authentication($window) {
+	authentication.$inject = ["$http, $window"];
+	function authentication($http, $window) {
 		var saveToken = function(token) {
 			$window.localStorage["runTrackerToken"] = token;
 		};
@@ -13,9 +13,52 @@
 			return $window.localStorage["runTrackerToken"] = token;
 		};
 
+		var isLoggedIn = function() {
+			var token = getToken();
+
+			if (token) {
+				var payload = JSON.parse($window.atob(token.split(".")[1]));
+				return payload.exp > Date.now() / 1000;
+			}else{
+				return false;
+			}
+		};
+
+		var currentUser = function() {
+			if (isLoggedIn()) {
+				var token = getToken();
+				var payload = JSON.parse($window.atob(token.split(".")[1]));
+				return {
+					email : payload.email,
+					name : payload.name
+				};
+			}
+		};
+
+		var register = function(user) {
+			return $http.post("/api/register", user).success(function(data) {
+				saveToken(data.token);
+			});
+		};
+
+		var login = function(user) {
+			return $http.post("/api/login", user).success(function(data) {
+				saveToken(data.token);
+			});
+		};
+
+		var logout = function() {
+			$window.localStorage.removeItem("runTrackerToken");
+		};
+
 		return {
+			currentUser : currentUser,
 			saveToken : saveToken,
-			getToken : getToken
+			getToken : getToken,
+			isLoggedIn : isLoggedIn,
+			register : register,
+			login : login,
+			logout : logout
 		};
 	}
 })();
